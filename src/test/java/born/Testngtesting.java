a410b654b1ea3661e6b0b4eeb2b521ba2164eb75
unknown <manoj.singh2410@gmail.com>
2023-03-22 00:18:54 +0530

    updated2

--- src/test/java/born/Testngtesting.java
+++ src/test/java/born/Testngtesting.java
@@ -54,7 +54,7 @@ public class Testngtesting {
 	        
 	        driver.get(baseUrl);
 	        //SoftAssert test= new SoftAssert();
-	        String testTitle = "Tools QA1";
+	        String testTitle = "Tools QA2";
 	        String originalTitle = driver.getTitle();	        
 	        assertEquals(testTitle, originalTitle);
 	       // test.assertEquals(originalTitle, testTitle);    
